01d75d72120d6f1a7d1b25ac481d7d7b90211aff
Prisca Ekkens <pekkens@mac.com>
2015-10-04 01:18:21 -0700

    updated

--- WeekThreeLabPathAnimation/ViewController.swift
+++ WeekThreeLabPathAnimation/ViewController.swift
@@ -9,33 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var plusImageView: UIImageView!
     @IBOutlet weak var plusBtn: UIButton!
-    @IBOutlet weak var playBtn: UIButton!
-    
-    
-    
+    @IBOutlet weak var playImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playBtn.alpha = 0
+        playImageView.alpha = 0
     }
-
+    
     @IBAction func onAddTapBtn(sender: AnyObject) {
         
-        //set starting point, don't forget to set alpha at 1
-        UIButton.animateWithDuration(1.0) { () -> Void in
-            self.playBtn.frame.origin.y = 350
-            self.playBtn.alpha = 1
-            print("play btn fired")
-            
-        //animate with rotation
-            self.playBtn.transform = CGAffineTransformMakeRotation(CGFloat(480 * M_PI / 180))
+        
+        //1.set starting point, don't forget to set alpha at 1
+        //2.animate with duration for time + completion; hit tab and enter to create areas for code
+        //3.animate plus button with seperat animate with duration
+        //4.concat so values for each animation don't conflict; create variables that make each action discret
+        //5.add Spring: damping is like wobble; velocity is like strength
+        
+        let rotateTransform = CGAffineTransformMakeRotation(CGFloat (135 * M_PI / 180))
+        let scaleTransform = CGAffineTransformMakeScale(1.2, 1.2)
+        let transform = CGAffineTransformConcat (rotateTransform, scaleTransform)
+        
+        //6.use center instead of origin to reset animation for each press
+        playImageView.center.y = plusImageView.center.y
+        
+        UIView.animateWithDuration(0.8, delay: 0.1, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.playImageView.center.y = 350
+            self.playImageView.alpha = 1
+            self.playImageView.transform = CGAffineTransformMakeRotation(CGFloat(120 * M_PI / 180))
+            }) { (Bool) -> Void in
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.plusImageView.transform = transform
+                })
         }
         
-        
     }
-
 }
+
 
